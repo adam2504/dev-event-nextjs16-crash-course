0ab3b70a48c80a903c97f06ff7ab446763252441
adam2504 <adam.jouini+publicis@publicismedia.com>
2026-01-06 15:48:02 +0100

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+  {
+    title: "React Conf 2026",
+    image: "/images/event1.png",
+    slug: "react-conf-2026",
+    location: "Las Vegas, USA",
+    date: "May 15-17, 2026",
+    time: "9:00 AM - 6:00 PM"
+  },
+  {
+    title: "Vue.js London",
+    image: "/images/event2.png",
+    slug: "vue-js-london-2026",
+    location: "London, UK",
+    date: "March 20-21, 2026",
+    time: "10:00 AM - 5:00 PM"
+  },
+  {
+    title: "Angular Connect",
+    image: "/images/event3.png",
+    slug: "angular-connect-2026",
+    location: "London, UK",
+    date: "September 8-9, 2026",
+    time: "9:30 AM - 6:30 PM"
+  },
+  {
+    title: "JSWorld Conference",
+    image: "/images/event4.png",
+    slug: "jsworld-conference-2026",
+    location: "Amsterdam, Netherlands",
+    date: "June 2-5, 2026",
+    time: "8:00 AM - 7:00 PM"
+  },
+  {
+    title: "Python Web Conf",
+    image: "/images/event5.png",
+    slug: "python-web-conf-2026",
+    location: "Munich, Germany",
+    date: "October 12-14, 2026",
+    time: "9:00 AM - 5:00 PM"
+  },
+  {
+    title: "DevOps Days",
+    image: "/images/event6.png",
+    slug: "devops-days-2026",
+    location: "Berlin, Germany",
+    date: "April 22-23, 2026",
+    time: "10:00 AM - 6:00 PM"
+  }
+];
